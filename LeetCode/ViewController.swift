55c4c5d2d30f170ab664fcbb1cda09d12db580f4
Ed <Siniakov>
2021-06-11 11:20:02 +0300

    Initial Commit

---
+++ LeetCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeetCode
+//
+//  Created by Eduard Sinyakov on 11.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
